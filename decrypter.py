7dea082a8d93572f62fa4d9a21b03b82009bd568
André Ferreira da Cruz <64928745+FerreirAndre@users.noreply.github.com>
2024-12-18 23:46:59 -0300

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,30 @@
+import sys
 import os
 import pyaes
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+def main():
+    if len(sys.argv) <= 1:
+        print("you need to pass a file as an argument")
+        return
 
-## chave para descriptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
+    file_name = sys.argv[1]
+    file = open(file_name, "rb")
+    file_data = file.read()
+    file.close()
 
-## remover o arquivo criptografado
-os.remove(file_name)
+    ## chave para descriptografia
+    key = str.encode("testeransomwares")
+    aes = pyaes.AESModeOfOperationCTR(key)
+    decrypt_data = aes.decrypt(file_data)
 
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
-new_file.close()
+    ## remover o arquivo criptografado
+    os.remove(file_name)
+
+    ## criar o arquivo descriptografado
+    new_file = f"{file_name.split(".")[0]}.txt"
+    new_file = open(f'{new_file}', "wb")
+    new_file.write(decrypt_data)
+    new_file.close()
+
+if __name__ == "__main__":
+    main()
